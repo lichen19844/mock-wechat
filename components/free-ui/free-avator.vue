ac82f93dd15d262e9d44a25ab0e77249e154a5ec
lichen <119449234@qq.com>
2020-05-26 23:37:20 +0800

    add avator

---
+++ components/free-ui/free-avator.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="flex align-center justify-center" style="width:145rpx;">
+		<image :src="src" mode="widthFix" :class="type" :style="getStyle"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			size: {
+				type: [String, Number],
+				default: 90
+			},
+			src: {
+				type: String,
+				default: ''
+			},
+			type: {
+				type: String,
+				default: 'rounded'
+			}
+		},
+		computed: {
+			getStyle () {
+				return `width: ${this.size}rpx; height: ${this.size}rpx`
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
